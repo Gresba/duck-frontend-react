ee7297827258ea44a04b1af8f55403c3a2e63e39
gresba <kimpaul0709@gmail.com>
2022-08-02 14:35:46 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+      <div>
+          <header>
+              <h1>
+                  Duck Menu
+              </h1>
+          </header>
+      </div>
+  );
+}
+
+export default App;
